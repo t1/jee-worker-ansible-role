d17a2d2f70871e997cf3932a35fb59f3862ac6a0
Rüdiger zu Dohna <snackbox@sinntr.eu>
2019-11-15 14:22:44 +0100

    extract role from deployer to publish on Ansible Galaxy

---
+++ molecule/tests/test_ee-worker.py
@@ -0,0 +1,15 @@
+import \
+    os
+
+import \
+    testinfra.utils.ansible_runner
+
+testinfra_hosts = testinfra.utils.ansible_runner.\
+    AnsibleRunner(os.environ['MOLECULE_INVENTORY_FILE']).\
+    get_hosts('all')
+
+
+# This test is quite unnecessary, as the role already does better checks
+def test_deployer_is_installed(host):
+    marker = '/opt/wildfly/standalone/deployments/deployer.war.deployed'
+    assert host.file(marker).is_file
